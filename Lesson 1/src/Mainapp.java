82e6cfbc739a6d1b7b2228b958a2f5501018ac30
daryabarkova <34532532+daryabarkova@users.noreply.github.com>
2017-12-14 09:00:16 +0300

    Add files via upload
    First commit

---
+++ Lesson 1/src/Mainapp.java
@@ -0,0 +1,139 @@
+
+public class Mainapp {
+
+	public static void main(String[] args) {
+			
+			 
+				
+		//1. Вывести на экран все четные цифры от 1 до 10 
+				
+		
+//		for(int i = 1; i < 10; i++){
+//						
+//			if (i%2==0) {
+//				System.out.println(i);
+//			}
+		
+//		boolean b1 = true;
+//		int j = 3;
+//		while(b1) {
+//			j--;
+//			if (j==1){
+//				b1 = false
+//			}
+//			System.out.println(j);
+//			j--;
+//		}
+//		
+//		do{
+//			
+//		}while(b1);
+		
+//		}
+		
+		
+		//2. Вывести на экран все четные цифры от 1 до 100, используя только условие цикла for
+		
+//		for(int i = 2; i <= 100; i=i+2){
+//			
+//			System.out.println(i);
+//		}
+		
+//		boolean b1 = true;
+//		int j = 100;
+//		while(b1) {
+//			j--;
+//			if (j%2==0) {
+//				b1 = false;
+//			}
+//			System.out.println(j);
+//		}
+		
+		int i = 2;
+		while (i <= 100) {
+		
+		System.out.println(i);
+		i = i+2;
+		
+		}
+		
+		
+		//3. Посчитать сумму цифр от 20 до 100
+		
+//		int s = 0;			
+//		for(int i = 20; i <= 100; i++){
+//			 s = s +i;
+//			
+//		}
+//		System.out.println(s);
+		
+//		boolean b1 = true;
+//		int i = 1;
+//		int s = 0;
+//		while (b1) {
+//			s = s + i;
+//			if (i == 100) {
+//			b1 = false;
+//			i++;
+//		}
+//		System.out.println(s);
+//		
+//		}
+		
+		//4. Пройти циклом от 1 до 12 и вывести название месяца, которое соответствует цифре
+		
+//		String month = "Jan";
+//		char letter = 'c';
+		
+//		int i;
+//		for(i = 1; i <= 12; i++){
+//			
+//			if (i == 1) {
+//			System.out.println("January");
+//		}
+//			if (i == 2) {
+//				System.out.println("February");	
+//			}
+//			if (i == 3) {
+//				System.out.println("March");
+//			}
+//			
+//		
+//		}
+		
+//		int i = 1;
+//		while (i<=12) {
+//			if (i == 1) {
+//				System.out.println("January");
+//			}
+//				if (i == 2) {
+//					System.out.println("February");	
+//				}
+//				if (i == 15) {
+//					System.out.println("March");
+//				}
+//				i++;
+//
+//	}
+		
+//		int i = 1;
+//		do {
+//			if (i == 1) {
+//				System.out.println("January");
+//			}
+//				if (i == 2) {
+//					System.out.println("February");	
+//				}
+//				if (i == 15) {
+//					System.out.println("March");
+//				}
+//				i++;
+//			
+//		} while (i<=12);
+}
+}
+
+ 
+
+
+
