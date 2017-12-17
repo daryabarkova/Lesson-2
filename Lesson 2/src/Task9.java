d65709bb3c6bb25a06fdb6a4b4c1f4a2897c6b76
daryabarkova <34532532+daryabarkova@users.noreply.github.com>
2017-12-17 22:09:23 +0300

    Add files via upload

--- Lesson 2/src/Task9.java
+++ Lesson 2/src/Task9.java
@@ -6,18 +6,14 @@ public class Task9 {
 
 	public static void main(String[] args) throws IOException {
 		// 9. Реализовать считывание фио для задачи 8 с клавиатуры
-		
-        char x[] = new char[13]; 
-        
-        String name = "";
-        	
+		                          	
 		BufferedReader keyb = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("Enter a line:");
-        System.out.println(keyb.readLine());  
+                
+        String name = keyb.readLine();
         
-        name = keyb.lines().collect(Collectors.joining("\n"));
-        
-                  
-	}
+        char x[] = name.toCharArray();
+         
+     }
 
 }
